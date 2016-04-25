9c4fdb9403a20662d0a0505a97a6fe94fcc2f9a9
Iñaki <iduetxe@gmail.com>
2016-04-25 10:33:08 +0200

    unused js function

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -278,14 +278,5 @@ var display = {
     }
 }
 
-function footerBottom(){
-    var docHeight = $(window).height();
-    var footerHeight = $('#footer').height();
-    var footerTop = $('#footer').position().top + footerHeight;
-
-    if (footerTop < docHeight) {
-        $('#footer').css('margin-top', 10+ (docHeight - footerTop) + 'px');
-    }
-}
 
 
